e237228bf23e2a0f2b34338ca408b8dc0fb7ad9f
la7ci9 <akarmiis997@gmail.com>
2022-09-07 13:33:15 +0200

    add annotations

--- src/main/java/com/shop/customer/Customer.java
+++ src/main/java/com/shop/customer/Customer.java
@@ -1,8 +1,19 @@
 package com.shop.customer;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.shop.shoppingcart.ShoppingCart;
 
+@Entity
+@Table(name = "shopcustomer")
 public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
