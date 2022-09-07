5d51ba795185edd9bb34eb5ef12399d4303cae6a
la7ci9 <akarmiis997@gmail.com>
2022-09-07 15:00:22 +0200

    create fields and methods

--- src/main/java/com/shop/shoppingcart/ShoppingCart.java
+++ src/main/java/com/shop/shoppingcart/ShoppingCart.java
@@ -1,5 +1,37 @@
 package com.shop.shoppingcart;
 
-public class ShoppingCart {
+import java.util.List;
+import com.shop.entity.Product;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "shoppingcart")
+public class ShoppingCart {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; 
+	private List<Product> customerCart;
+	
+	public ShoppingCart(Long id, List<Product> customerCart) {
+		this.id = id;
+		this.customerCart = customerCart;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public List<Product> getCustomerCart() {
+		return customerCart;
+	}
+	public void setCustomerCart(List<Product> customerCart) {
+		this.customerCart = customerCart;
+	}
 }
