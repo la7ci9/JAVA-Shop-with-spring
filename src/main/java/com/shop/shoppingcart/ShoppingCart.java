ba417e3237705f9104c2da1a8b57c9952834d3d8
la7ci9 <akarmiis997@gmail.com>
2022-09-08 10:27:23 +0200

    added customer field

--- src/main/java/com/shop/shoppingcart/ShoppingCart.java
+++ src/main/java/com/shop/shoppingcart/ShoppingCart.java
@@ -1,6 +1,8 @@
 package com.shop.shoppingcart;
 
 import java.util.List;
+
+import com.shop.customer.Customer;
 import com.shop.entity.Product;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,18 +17,27 @@ public class ShoppingCart {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id; 
+	private Customer customer;
 	private List<Product> customerCart;
 	
 	public ShoppingCart(Long id, List<Product> customerCart) {
 		this.id = id;
 		this.customerCart = customerCart;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+	
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 	public List<Product> getCustomerCart() {
 		return customerCart;
