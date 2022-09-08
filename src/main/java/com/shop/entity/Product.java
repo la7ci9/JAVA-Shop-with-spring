ca0da24251cac06ff287619195360431e78ac2a0
la7ci9 <akarmiis997@gmail.com>
2022-09-08 10:29:56 +0200

    added annotations

--- src/main/java/com/shop/entity/Product.java
+++ src/main/java/com/shop/entity/Product.java
@@ -1,7 +1,17 @@
 package com.shop.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nameOfProduct;
 	private Double price;
