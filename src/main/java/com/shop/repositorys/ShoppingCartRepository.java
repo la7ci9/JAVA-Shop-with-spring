7f68e7d08c08d7fc0f0691f4001f53ffc90bdb0c
la7ci9 <akarmiis997@gmail.com>
2022-09-09 20:02:42 +0200

    modify to shoppingcartitemrepository

--- src/main/java/com/shop/repositorys/ShoppingCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shop.repositorys;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shop.shoppingcart.ShoppingCart;
-
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-
-}
