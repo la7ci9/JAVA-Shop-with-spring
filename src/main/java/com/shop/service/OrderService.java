bcaa9d1bd1635ff26cc013b697394bbe4a43bdd9
la7ci9 <akarmiis997@gmail.com>
2022-09-20 19:47:01 +0200

    added fields and constructor

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -3,10 +3,20 @@ package com.shop.service;
 import java.util.List;
 
 import com.shop.order.Order;
+import com.shop.repositorys.OrderRepository;
+import com.shop.repositorys.ProductRepository;
 import com.shop.serviceDAO.OrderServiceDAO;
 import com.shop.shoppingcart.CartItem;
 
 public class OrderService implements OrderServiceDAO {
+	
+	OrderRepository orderRepository;
+	ProductRepository proudctRepository;
+	
+	public OrderService(OrderRepository orderRepository, ProductRepository proudctRepository) {
+		this.orderRepository = orderRepository;
+		this.proudctRepository = proudctRepository;
+	}
 
 	@Override
 	public Order getOrderDetail(Long orderId) {
