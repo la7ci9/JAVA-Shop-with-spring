a4ba86b1f190474b550d001e10a2ac458f332dcc
la7ci9 <akarmiis997@gmail.com>
2022-09-21 14:17:15 +0200

    added finding method

--- src/main/java/com/shop/repositorys/CustomerRepository.java
+++ src/main/java/com/shop/repositorys/CustomerRepository.java
@@ -6,4 +6,5 @@ import com.shop.customer.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
+	public Customer findByNameAndEmail(String firstName, String lastName, String email);
 }
