3cd3b3ded6809109ceaf6c9081b26fe4fd800e81
la7ci9 <akarmiis997@gmail.com>
2022-10-11 15:46:58 +0200

    added annotations

--- src/main/java/com/shop/order/Order.java
+++ src/main/java/com/shop/order/Order.java
@@ -2,24 +2,41 @@ package com.shop.order;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import com.shop.customer.Customer;
 import com.shop.shoppingcart.CartItem;
 
+@Entity
+@Table(name = "Orders")
 public class Order {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@OneToOne(cascade = CascadeType.MERGE)
 	private Customer customer;
-	private String orderDescriptiion;
+	private String orderDescription;
+	
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	List<CartItem> orderCartItems;
 	
 	public Order() {
 		//empty consturctor
 	}
 
-	public Order(Customer customer, String orderDescriptiion,
+	public Order(Customer customer, String orderDescription,
 			List<CartItem> orderCartItems) {
 		this.customer = customer;
-		this.orderDescriptiion = orderDescriptiion;
+		this.orderDescription = orderDescription;
 		this.orderCartItems = orderCartItems;
 	}
 
@@ -39,12 +56,12 @@ public class Order {
 		this.customer = customer;
 	}
 
-	public String getOrderDescriptiion() {
-		return orderDescriptiion;
+	public String getOrderDescription() {
+		return orderDescription;
 	}
 
-	public void setOrderDescriptiion(String orderDescriptiion) {
-		this.orderDescriptiion = orderDescriptiion;
+	public void setOrderDescriptiion(String orderDescription) {
+		this.orderDescription = orderDescription;
 	}
 
 	public List<CartItem> getOrderCartItems() {
